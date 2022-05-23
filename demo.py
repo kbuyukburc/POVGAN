c69d62e0d12a605ee91a127d730ee6e25208b944
kbuyukburc <kbuyukburc@gmail.com>
2022-05-23 13:56:01 +0800

    FEAT: Cameras extrinsic is added.

--- demo.py
+++ demo.py
@@ -26,11 +26,35 @@ from scipy.spatial.transform import Rotation as R
 from pyquaternion import Quaternion
 
 model = create_model(opt)
-nuscenes_camera_extrinsic = \
-    {
-1: {"translation": [1.70079118954,0.0159456324149,1.51095763913],
-"rotation": [0.4998015430569128,-0.5030316162024876,0.4997798114386805,-0.49737083824542755]}
+nuscenes_lidar_extrinsic = {
+    "LIDAR_TOP":{
+    "translation": [0.943713,0.0,1.84023],
+    "rotation": [0.7077955119163518,-0.006492242056004365,0.010646214713995808,-0.7063073142877817]
     }
+}
+
+nuscenes_camera_extrinsic = {
+    1:{
+    "translation": [1.70079118954,0.0159456324149,1.51095763913],
+    "rotation": [0.4998015430569128,-0.5030316162024876,0.4997798114386805,-0.49737083824542755]
+    },
+    2:{
+    "translation": [0.0283260309358,0.00345136761476,1.57910346144],
+    "rotation": [0.5037872666382278,-0.49740249788611096,-0.4941850223835201,0.5045496097725578]
+    },
+    3:{"translation": [1.5508477543,-0.493404796419,1.49574800619],
+    "rotation": [0.2060347966337182,-0.2026940577919598,0.6824507824531167,-0.6713610884174485]
+    },
+    4:{"translation": [1.52387798135,0.494631336551,1.50932822144],
+    "rotation": [0.6757265034669446,-0.6736266522251881,0.21214015046209478,-0.21122827103904068],
+    },
+    5:{"translation": [1.0148780988,-0.480568219723,1.56239545128],
+    "rotation": [0.12280980120078765,-0.132400842670559,-0.7004305821388234,0.690496031265798],
+    },
+    6:{"translation": [1.52387798135,0.494631336551,1.50932822144],
+    "rotation": [0.6757265034669446,-0.6736266522251881,0.21214015046209478,-0.21122827103904068],
+    },
+}
 
 def calibDictToMatrix(calib_dict : dict) -> np.ndarray:
     quat = Quaternion(calib_dict['rotation'])
@@ -47,6 +71,7 @@ class BinDataset:
         self.bin_files = list(folder.glob('*.bin'))
 
     def __getitem__(self, index):
+        index = index % len(self.bin_files)
         return np.fromfile(self.bin_files[index], dtype=np.float32).reshape(-1,5)[..., :4]
 
 class PointCloudViewer:
@@ -69,15 +94,27 @@ class PointCloudViewer:
     def __init__(self, dataset):
         self.dataset = dataset        
         self.app.register_key_callback(ord('Q'), self.__close_app)
-        for i in range(1,2):
+        self.app.register_key_callback(ord('D'), self.__next_frame)
+        self.app.register_key_callback(ord('A'), self.__back_frame)
+        for i in range(1,7):
             self.app.register_key_callback(ord(f'{i}'), self.__get_camera(i))
+
+    def __next_frame(self, app):
+        self.index += 1
+        self.get_frame()
+
+    def __back_frame(self, app):
+        self.index -= 1
+        self.get_frame()
 
     def __get_camera(self, number):
         def get_camera(app):
             cntrl = app.get_view_control()
             param = o3d.camera.PinholeCameraParameters()
             param.intrinsic = cntrl.convert_to_pinhole_camera_parameters().intrinsic
-            param.extrinsic = calibDictToMatrix(nuscenes_camera_extrinsic[number])
+            camera_extrinsic = calibDictToMatrix(nuscenes_camera_extrinsic[number])
+            lidar_extrinsic = calibDictToMatrix(nuscenes_lidar_extrinsic["LIDAR_TOP"])
+            param.extrinsic = np.linalg.inv(camera_extrinsic) @ lidar_extrinsic
             cntrl.convert_from_pinhole_camera_parameters(param)
         return get_camera
 
